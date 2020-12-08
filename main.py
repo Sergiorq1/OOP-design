393c22a0d5b0613fb39655963e335cf6ac6ec9e3
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2020-12-08 08:40:38 -0800

    moves objects

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 import random
+
 #initialize pygame
 pygame.init()
 
@@ -10,10 +11,93 @@ screen = pygame.display.set_mode((800,600))
 icon = pygame.image.load('spaceship.png')
 pygame.display.set_caption("Space Invaders")
 pygame.display.set_icon(icon)
-# Bakcground
+# Background
 background = pygame.image.load("space-galaxy-background.jpg")
 #game loop
 running = True
+
+
+class Player:
+    def __init__(self, x, y, xChange, yChange):
+        self.x = x
+        self.y = y
+        self.xChange = xChange
+        self.yChange = yChange
+    def appear(self):
+        playerImg = pygame.image.load("shooter.png")
+        screen.blit(playerImg, (self.x, self.y))
+        self.x += self.xChange
+    def move(self):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.xChange = -7
+            if event.key == pygame.K_RIGHT:
+                self.xChange = 7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                self.xChange = 0
+
+shootie = Player(370,480,0,0)
+
+class Enemy():
+    def __init__(self, x, y, xChange, yChange):
+        self.x = x
+        self.y = y
+        self.xChange = 5
+        self.yChange = 40
+    def randSpawn(self):
+        self.x = random.randint(0,735)
+        self.y = random.randint(50, 150)        
+    def move(self):
+        enemyImg = pygame.image.load("alien.png")
+        screen.blit(enemyImg, (self.x, self.y))
+        self.x += self.xChange
+        if self.x <= 0:
+            self.xChange = 5
+            self.y += self.yChange
+        elif self.x >= 736:
+            self.xChange = -5
+            self.y += self.yChange
+alien = Enemy(0,0,5,40)
+
+class Astroids(Enemy):
+    def randSpawn(self):
+        if self.y == -90:
+            self.x = random.randint(0, 735)
+    def move(self):
+        rockImg = pygame.image.load("rock.png")
+        screen.blit(rockImg, (self.x, self.y))
+        self.yChange = 10
+        self.y += self.yChange
+        if self.y >= 900:
+            self.y = -90
+    
+
+rock = Astroids(0, 0, 5, 0)
+class Bullet:
+    def __init__(self, x, y, yChange, state):
+        self.x = x
+        self.y = y
+        self.yChange = yChange
+        self.state = "ready"
+    def fire(self):
+        self.state = "fire"
+        self.x = shootie.x
+
+    def move(self):
+        bulletImg = pygame.image.load("bullet.png")
+        screen.blit(bulletImg, (self.x + 16, self.y + 10))        
+        if self.state == "fire":
+            self.y -= self.yChange
+        if self.y <= 0:
+            self.y = 480
+            self.state = "ready"
+            self.x = -50
+
+bullet = Bullet(-50, 480, 10, "ready")
+
+rock.randSpawn()
+alien.randSpawn()
 while running:
     screen.fill((0,0,0))  
     #Background image
@@ -22,6 +106,20 @@ while running:
         #to close window
         if event.type == pygame.QUIT:
             running = False
-
+        shootie.move()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bullet.fire()
+    
+    rock.move()
+    bullet.move()
+    
+    alien.move()
+    shootie.appear()
+    
     #Updates each frame
     pygame.display.update()
+
+
+
+
